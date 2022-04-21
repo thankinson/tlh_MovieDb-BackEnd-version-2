f913775f4cb6c52563a9614836ab5dfae48be766
thankinson <t_hankinson@outlook.com>
2022-04-21 11:23:49 +0100

    added list user movies and add to movies

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { addUser, login, updatePassword, deleteUser, listUser  } = require("./userControlers");
+const { addUser, login, updatePassword, deleteUser, listUser, addFilm, listFilms  } = require("./userControlers");
 const { hashPassword, decryptPassword, checkToken } = require("../middleware");
 const userRouter = Router();
 
@@ -9,5 +9,8 @@ userRouter.get("/user", checkToken, login);
 userRouter.get("/login", listUser)
 userRouter.patch("/user", hashPassword, checkToken, updatePassword);
 userRouter.delete("/user/:filterVal/:filterKey", deleteUser);
+
+userRouter.get("/films", checkToken, listFilms);
+userRouter.put("/addtitle", addFilm);
 
 module.exports = userRouter;
